c943f6c917001ed8dd88ced645f0ae253560b8da
jtauaikat <toumujo@whs.school.nz>
2022-05-11 12:15:22 +1200

    work done on 11/05/22

--- MainGame.java
+++ MainGame.java
@@ -6,73 +6,93 @@
  * 09/05/22
  */
 import java.util.Random;
-
+import java.util.Arrays;
 public class MainGame
 {
     Random rand = new Random();
-    public int x = 0;
-    public int y = 0;
-    public int ROWS = 20;
-    public int COLUMNS = 20;
-    public int temp[][] = new int[COLUMNS][ROWS];
-    public int board[][] = new int[COLUMNS][ROWS];
+    
+    public int SIZE = 20;
+    public int temp[][] = new int[SIZE][SIZE];
+    //public int board[][] = new int[SIZE][SIZE];
+    public int board[][] = 
+        {{0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0}};
+    public int generations = 5;
     public int totalNeighbors = 0;
     public int toggle=0;
     public MainGame()
     {
-        System.out.println('\u000c');
-        for(int y = 0; y<COLUMNS; y++){
-            for(int x = 0; x<ROWS; x++){
-                board[y][x] = (rand.nextInt(2))*(rand.nextInt(2));
-            }
-        }
+        System.out.print('\u000c');
+        //for(int y = 0; y<SIZE; y++){
+        //  for(int x = 0; x<SIZE; x++){
+        //      board[y][x] = (rand.nextInt(2))*(rand.nextInt(2));
+        //  }
+        //}
         System.out.println("------------------------------------");
         while(toggle<5){
-        for(y = 0; y<COLUMNS; y++){
-            for(x = 0; x<ROWS; x++){
-                gameRules();
-                System.out.print(board[y][x] + " ");
+            for(int y = 0; y<SIZE; y++){
+                for(int x = 0; x<SIZE; x++){
+                    gameRules(x, y);
+                    System.out.print(board[y][x] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
+            arrayClone();
+            System.out.println("------------------------------------");
+            toggle++;
         }
-        arrayClone();
-        System.out.println("------------------------------------");
-        toggle++;
-    }
     }
 
-    void gameRules()
+    void gameRules(int xIn,int yIn)
     {
         for(int yMod = -1; yMod<2;yMod++){
             for(int xMod = -1; xMod<2; xMod++ ){
-                if(x+xMod != x && y+yMod != y &&  x+xMod >= 0 && y+yMod >= 0 && x+xMod <= ROWS && y+yMod <= COLUMNS){
-                    totalNeighbors += board[y+yMod][x+xMod];
+                if(xMod+yMod != 0 &&  xIn+xMod >= 0 && yIn+yMod >= 0 && xIn+xMod < SIZE && yIn+yMod < SIZE){
+                    totalNeighbors += board[yIn+yMod][xIn+xMod];
                 }
             }
         }
-        if(board[y][x] == 1){
+        if(board[yIn][xIn] == 1){
             if(totalNeighbors<2){
-                temp[y][x] = 0;
+                temp[yIn][xIn] = 0;
             }
-            else if(totalNeighbors>=2 || totalNeighbors<=3){
-                temp[y][x] = 1;
+            else if(totalNeighbors>=2 && totalNeighbors<=3){
+                temp[yIn][xIn] = 1;
             }
             if(totalNeighbors>3){
-                temp[y][x] = 0;
+                temp[yIn][xIn] = 0;
             }
-        }else if(board[y][x] == 0){
+        }else if(board[yIn][xIn] == 0){
             if(totalNeighbors == 3){
-                temp[y][x] = 1;
+                temp[yIn][xIn] = 1;
             }else{
-                temp[y][x] = 0;
+                temp[yIn][xIn] = 0;
             }
         }
     }
-    
+
     void arrayClone()
     {
-        for(int yClone = 0; yClone<COLUMNS; yClone++){
-            for(int xClone = 0; xClone<ROWS; xClone++){
+        for(int yClone = 0; yClone<SIZE; yClone++){
+            for(int xClone = 0; xClone<SIZE; xClone++){
                 board[yClone][xClone] = temp[yClone][xClone];
             }
         }
